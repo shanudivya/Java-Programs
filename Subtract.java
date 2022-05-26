0f091054eb16e3f0883c41c5b1744542a5537595
Divya Gupta <shanu.divya123@gmail.com>
2022-05-26 18:11:37 +0530

    Add files via upload

---
+++ Subtract.java
@@ -0,0 +1,9 @@
+public class Subtract {
+    public static void main (String args[]){
+        int a=10;
+        int b=5;
+        int subtract;
+        subtract=a-b;
+        System.out.println("Subtraction of a and b is:" +subtract);
+    }
+}
