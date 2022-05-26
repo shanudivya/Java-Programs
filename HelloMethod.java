32ac718ee6d599bcab6e023faf5822aef8e2a06e
Divya Gupta <shanu.divya123@gmail.com>
2022-05-26 17:46:28 +0530

    Add files via upload

---
+++ HelloMethod.java
@@ -0,0 +1,9 @@
+public class HelloMethod {
+    public static void main (String args[]){
+        System.out.println(HelloMethod());
+    }
+    public static String HelloMethod(){
+        return "Hello World";
+    }
+    
+}
