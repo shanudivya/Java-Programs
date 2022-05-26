1d9b600dc96d5722f1a99e1b14136b4fa45a4cda
Divya Gupta <shanu.divya123@gmail.com>
2022-05-26 17:52:36 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,10 @@
+public class Addition {
+    public static void main (String args[]){
+        int a=4;
+        int b=6;
+        int sum;
+        sum=a+b;
+        System.out.println("sum of a and b is:" +sum);
+    }
+    
+}
