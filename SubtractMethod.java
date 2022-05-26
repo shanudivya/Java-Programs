681480c77a092bdbb0c0beab9bdb1b98d68583f7
Divya Gupta <shanu.divya123@gmail.com>
2022-05-26 18:14:50 +0530

    Add files via upload

---
+++ SubtractMethod.java
@@ -0,0 +1,12 @@
+public class SubtractMethod {
+    public static void main (String args[]){
+        int a=10;
+        int b=5;
+      
+        System.out.println(SubtractMethod(a,b));
+    }
+    public static int SubtractMethod(int a, int b){
+        return a-b;
+    }
+    
+}
