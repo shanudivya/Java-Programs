a0f5d5b6859b6181cd4370f63d1704dfdff5aeb4
Divya Gupta <shanu.divya123@gmail.com>
2022-05-26 18:05:08 +0530

    Add files via upload

---
+++ AdditionMethod.java
@@ -0,0 +1,11 @@
+public class AdditionMethod {
+    public static void main(String args[]){
+        int a=4;
+        int b=6;
+        int sum;
+        System.out.println(AdditionMethod(a,b));
+    }
+    public static int AdditionMethod(int a, int b){
+        return a+b;
+    }
+}
